ffc66cb43e41b06c6038cacb2e5ca6f71efa0842
kapilsantilal <kapilbamania96@gmail.com>
2023-12-19 20:36:01 +0000

    Homework/home-office-visa-check-sw6

---
+++ src/test/java/gov/uk/check/visa/steps/CountryDropDownSteps.java
@@ -0,0 +1,14 @@
+package gov.uk.check.visa.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class CountryDropDownSteps {
+    @Given("I am on homepage")
+    public void iAmOnHomepage() {
+    }
+
+    @Then("I can see following {string} into dropdown")
+    public void iCanSeeFollowingCountryIntoDropdown() {
+    }
+}
